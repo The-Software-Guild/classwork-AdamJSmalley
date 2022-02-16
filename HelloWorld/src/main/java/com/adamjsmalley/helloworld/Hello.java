3c57f94716901e7695f68c20be3d879017a49047
Adam Smalley <adamjs16@gmail.com>
2022-02-16 08:08:59 +0000

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/com/adamjsmalley/helloworld/Hello.java
@@ -0,0 +1,16 @@
+/**
+*@author Adam Smalley
+*email: adamjs16@gmail.com
+*date: 15/02/2022
+*purpose: My first Hello World program in an IDE
+*/
+
+
+package com.adamjsmalley.helloworld;
+
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Adam Smalley");
+    }
+}
